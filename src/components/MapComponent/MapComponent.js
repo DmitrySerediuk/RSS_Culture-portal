62e7b901b98fe9ee32ce9a1fd14c21ff2e682a0c
gebodya <getbodya.0@gmail.com>
2019-07-30 00:56:48 +0300

    feat: implementation map marker

--- src/components/MapComponent/MapComponent.js
+++ src/components/MapComponent/MapComponent.js
@@ -2,28 +2,29 @@ import React, { useState } from "react";
 import GoogleMapReact from 'google-map-react';
 import { Map, Marker, MarkerLayout } from 'yandex-map-react';
 
+import MapMarker from './MapMarker'
 
 export default function MapComponent(){
-    const defaultProps = {
-        center: {
-          lat: 59.95,
-          lng: 30.33
-        },
-        zoom: 11
-      };
+    const [state,setState]=useState({
+        markers:[
+            {
+                lat:53.89833,
+                lon:27.56056,
+                imgUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/13/%D0%A5%D1%83%D0%B4_%D0%BC%D1%83%D0%B7%D0%B5%D0%B9_%D0%A0%D0%91.jpg/250px-%D0%A5%D1%83%D0%B4_%D0%BC%D1%83%D0%B7%D0%B5%D0%B9_%D0%A0%D0%91.jpg'
+            }
+        ]
+    })
+    const {markers} = state;
+    const markerList = markers.map((marker,index)=>{
+        return <MapMarker marker={marker} key={index} />
+    })
+    const center = [markers[0].lat,markers[0].lon]
       return (
-        <Map style={{ height: '100vh', width: '100%' }}center={[55.754734, 37.583314]} zoom={10}>
-            <Marker lat={55.754734} lon={37.583314} />
+        <Map 
+        style={{ height: '100vh', width: '100%' }}
+        center={center} 
+        zoom={14}>
+            {markerList}
         </Map>
     );
-    // return(
-    //     <div style={{ height: '100vh', width: '100%' }}>
-    //     <GoogleMapReact
-    //       bootstrapURLKeys={{ key:'AIzaSyBAw16oVHPCI33-zwVfhySzWVPk3YXO2Vg' }}
-    //       defaultCenter={defaultProps.center}
-    //       defaultZoom={defaultProps.zoom}
-    //     >
-    //     </GoogleMapReact>
-    //     </div>
-    // )
 }
---
+++ src/components/MapComponent/MapMarker.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Marker, MarkerLayout } from 'yandex-map-react';
+
+export default function MapMarker(props) {
+    const { lat, lon, imgUrl } = props.marker;
+    const imgStyle = {
+        width: 30,
+        height: 30,
+        backgroundImage:`url(${imgUrl})`,
+        backgroundSize:'cover',
+        border:'blue solid 2px',
+        borderRadius: '50%',
+        overflow: 'hidden'
+    }
+    return (
+        <Marker lat={lat} lon={lon}>
+            <MarkerLayout>
+                <div style={imgStyle}>
+                </div>
+            </MarkerLayout>
+        </Marker>
+    )
+}
