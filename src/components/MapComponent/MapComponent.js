279d6e2b2f29263367d47ae5138d2602e4c71f67
gebodya <getbodya.0@gmail.com>
2019-07-30 00:12:52 +0300

    feat: implementation yandex map

--- src/components/MapComponent/MapComponent.js
+++ src/components/MapComponent/MapComponent.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import GoogleMapReact from 'google-map-react';
+import { Map, Marker, MarkerLayout } from 'yandex-map-react';
+
 
 export default function MapComponent(){
     const defaultProps = {
@@ -9,14 +11,19 @@ export default function MapComponent(){
         },
         zoom: 11
       };
-    return(
-        <div style={{ height: '100vh', width: '100%' }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key:'AIzaSyBAw16oVHPCI33-zwVfhySzWVPk3YXO2Vg' }}
-          defaultCenter={defaultProps.center}
-          defaultZoom={defaultProps.zoom}
-        >
-        </GoogleMapReact>
-        </div>
-    )
+      return (
+        <Map style={{ height: '100vh', width: '100%' }}center={[55.754734, 37.583314]} zoom={10}>
+            <Marker lat={55.754734} lon={37.583314} />
+        </Map>
+    );
+    // return(
+    //     <div style={{ height: '100vh', width: '100%' }}>
+    //     <GoogleMapReact
+    //       bootstrapURLKeys={{ key:'AIzaSyBAw16oVHPCI33-zwVfhySzWVPk3YXO2Vg' }}
+    //       defaultCenter={defaultProps.center}
+    //       defaultZoom={defaultProps.zoom}
+    //     >
+    //     </GoogleMapReact>
+    //     </div>
+    // )
 }
