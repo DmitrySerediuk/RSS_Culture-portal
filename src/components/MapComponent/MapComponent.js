25bd8d85ecd4e16e224bc50806c0e86e543a3d58
Maryia Larchanka <mashalarchenko16@gmail.com>
2019-08-02 00:25:41 +0300

    refactor: add more info in tool tip

--- src/components/MapComponent/MapComponent.js
+++ src/components/MapComponent/MapComponent.js
@@ -29,10 +29,11 @@ const useStyles = makeStyles(theme => ({
         padding: theme.spacing(3, 2),
         margin: '0 auto 20px'
     },
-    tooltip: {
-        position: 'relative',
-        width: 100,
-    }
+    // tooltip: {
+    //     position: 'relative',
+    //     width: 100,
+    //     height: 20%,
+    // }
 }));
 
 export default function MapComponent() {
@@ -43,7 +44,11 @@ export default function MapComponent() {
             lon: 27.56056,
             imgUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/13/%D0%A5%D1%83%D0%B4_%D0%BC%D1%83%D0%B7%D0%B5%D0%B9_%D0%A0%D0%91.jpg/250px-%D0%A5%D1%83%D0%B4_%D0%BC%D1%83%D0%B7%D0%B5%D0%B9_%D0%A0%D0%91.jpg',
             title: 'Национальный Художественный музей',
-            toolTip: { balloonContentBody: 'Национальный Художественный музей' },
+            toolTip: {
+                balloonContentHeader: 'Национальный Художественный музей',
+                balloonContentBody: 'Адрес: улица Ленина 20',
+                balloonContentFooter: 'Минск, Беларусь'
+            }
         },
         center: [53.89833, 27.56056],
     })
